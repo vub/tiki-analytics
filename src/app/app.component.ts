147e167d7dd02b7ef3d56c784b188a9f23de79ab
vu.bui <vu.bui@saigontechnology.com>
2020-01-29 10:49:31 +0700

    Add orders analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import filter from 'lodash/filter';
+import sumBy from 'lodash/sumBy';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tiki-analytics';
+  apiResult = '';
+
+  orders = {
+    data: [],
+    paging: {}
+  };
+  completedProject = [];
+  totalSpent = 0;
+  totalCompletedTransaction = 0;
+  totalCancel = 0;
+
+  updateResult() {
+    this.orders = JSON.parse(this.apiResult);
+    console.log(this.orders);
+    this.completedProject = filter(this.orders.data, order => {
+      return order.status === 'hoan_thanh';
+    });
+    this.totalSpent = sumBy(this.completedProject, order => {
+      return order.grand_total;
+    });
+    this.totalCompletedTransaction = this.completedProject.length;
+    const cancelOrder = filter(this.orders.data, order => {
+      return order.status === 'canceled';
+    });
+    this.totalCancel = cancelOrder.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
